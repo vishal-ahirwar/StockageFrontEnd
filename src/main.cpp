7da6aa93eec519d33b330f672c228a74902211cd
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2025-02-01 21:59:10 +0530

    G

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
-    engine.loadFromModule("Stockage", "Main");
+    engine.load("qrc:/qml/Main.qml");
 
     return app.exec();
 }
